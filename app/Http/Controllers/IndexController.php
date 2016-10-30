a55759692e0bcc3dc55934ab455070fdde3bfd1f
Chris Van Patten <chris@vanpattenmedia.com>
2016-10-30 18:23:03 -0400

    working account viewing

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client as Guzzle;
+
 class IndexController extends BaseController
 {
 
@@ -10,7 +12,16 @@ class IndexController extends BaseController
 	 */
 	public function get( $request, $response, $args )
 	{
-		return $this->view->render($response, 'pages/index.html.twig');
+		$client = new Guzzle();
+
+		$remote = $client->get('https://alexa.chrisvanpatten.com/alexa-bookkeeper/alexa-bookkeeper.php');
+		$accounts = json_decode($remote->getBody(), true);
+
+		$vars = [
+			'accounts' => $accounts,
+		];
+
+		return $this->view->render($response, 'pages/index.html.twig', $vars);
 	}
 
 }
