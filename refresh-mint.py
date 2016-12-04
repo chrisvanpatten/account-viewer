23b047f6423aca37e20d2ccc1b3a7dd2b170093e
Chris Van Patten <chris@vanpattenmedia.com>
2016-12-04 08:01:32 -0500

    small fixes for new parameters

--- refresh-mint.py
+++ refresh-mint.py
@@ -5,5 +5,5 @@ with open('config.json') as config:
     config = json.load(config)
 
 for account in config['accounts']:
-	mint = mintapi.Mint(account['email'], account['password'], account['session'])
+	mint = mintapi.Mint(account['email'], account['password'], account['session'], account['guid'])
 	mint.initiate_account_refresh()
